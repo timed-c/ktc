fb753fb34153db63f076c8f37ae67fdbb513bcde
root <nsaranya.anwendung@gmail.com>
2018-02-21 22:54:43 +0100

    firm ino

--- rtas/examples/firm.cc
+++ rtas/examples/firm.cc
@@ -1,6 +1,6 @@
 void main(){
   while(1){
-    every (30) 
+    every(30) 
       sense();//read from sensor	
   }
 } 
--- rtas/examples/firm.ino
+++ rtas/examples/firm.ino
@@ -1,20 +1,27 @@
 #include <setjmp.h>
 #include "TimerOne.h"
 jmp_buf env;
+unsigned long tinit = 0;
 void setup(){
-  Timer1.initialize(500000);  
+  Timer1.initialize();  
+  Timer1.setPeriod(30000);//30ms
   Timer1.attachInterrupt(callback);  
+  Timer1.start();
 }
 void callback(){
   longjmp(env, 2);
 }
 void loop() {
   int i;
+  tinit = millis();
   i = setjmp(env);
-  if (i != 0){ 
+  if (i != 0){
      goto lbl;
   }
   sense();//read from sensor
+  delay(30 - (millis() - tinit));
   lbl:
   Timer1.restart();
 }
+
+
