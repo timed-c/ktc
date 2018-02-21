7f25c1e46d72612e4959613ef1d9fad1544243bf
root <nsaranya.anwendung@gmail.com>
2018-02-21 19:59:25 +0100

    rtcc indentation

--- rtas/examples/firm.cc
+++ rtas/examples/firm.cc
@@ -1,7 +1,8 @@
 void main(){
-    while(1){
-        every (100) 
-	    sense();	
-  	else 
-            handle_deadline();
+  while(1){
+    every (30) 
+      sense();	
+    else 
+      handle_deadline();
+  }
 } 
