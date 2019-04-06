85d5f95c7300ac456f3902a9f9da4c7a27e6a7ed
root <nsaranya.anwendung@gmail.com>
2019-04-06 12:26:03 +0200

    offset fix tfg

--- src/tfg.ml
+++ src/tfg.ml
@@ -846,7 +846,7 @@ let lcm m n =
   | m, n -> abs (m * n) / (gcd m n)
 
 let calculateHyperperiod tlist =
-    let hyperiod = List.fold_right lcm (List.tl tlist) 1 in
+    let hyperiod = List.fold_right lcm (tlist) 1 in
     E.log "Hyperperiod %d\n" hyperiod; hyperiod
 
 (*
@@ -995,7 +995,7 @@ let findHyperperiod tlist alist =
     let task_arrival_pair = List.map (fun a -> ((List.nth a 0), (int_of_string
     (List.nth a 1)))) tlist in
     let task_list = List.map (fun a -> List.nth a 0) tlist in
-    let unique_task_arrival_pair = uniqueTaskPair task_list task_arrival_pair in
+    let unique_task_arrival_pair = uniqueTaskPair (List.tl task_list) task_arrival_pair in
     let hp = calculateHyperperiod (List.map (fun a -> (snd a))
     unique_task_arrival_pair) in
     let ncsv = List.map (to_csv_string) (unrollToHyper hp tlist task_list) in
