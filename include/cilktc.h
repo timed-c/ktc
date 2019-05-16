69e5174a4a9da081f94d32418a1a66fa35d0b76b
root <nsaranya.anwendung@gmail.com>
2019-05-16 23:08:24 +0200

    cilktc header file

--- include/cilktc.h
+++ include/cilktc.h
@@ -257,6 +257,13 @@ struct minmax_struct{
     int mdst;
 };
 
+void log_trace_init(const char* func, struct _IO_FILE *fp);
+void log_trace_init_tp(struct _IO_FILE *fp, int tp, long* arrival_init, struct timespec itime);
+void log_trace_arrival(FILE* fp, int tp, int interval, int res, long *last_arrival);
+void log_trace_release(FILE* fp, long last_arrival, struct timespec itime, struct timespec* stime, int interval);
+void log_trace_execution(FILE* fp, struct timespec stime);
+void log_trace_end_id(FILE* fp, int id, struct timespec stime);
+void log_trace_abort_time(FILE* fp);
 
 void plog_trace_init_tp(struct log_struct* fp, FILE* fptr, int tp, unsigned long* arrival_init, struct timespec itime);
 void plog_trace_init(const char* func, struct _IO_FILE *fp);
