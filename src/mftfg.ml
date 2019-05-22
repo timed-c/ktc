f45ae20a897204131d6d42fe371275af1f705701
root <nsaranya.anwendung@gmail.com>
2019-05-22 12:16:14 +0200

    deadline monotonic

--- src/mftfg.ml
+++ src/mftfg.ml
@@ -1114,8 +1114,8 @@ let findHyperperiod tlist alist jlist =
     let njblist = List.filter (fun [tid; jid; amin; amax; cmin; cmax; dl; pr;k] -> dl <> 0) ujblist in
     let ncsv = List.map (to_csv_string) (njblist) in
     (*let _ = E.log "new_win %d" new_win in*)
-    let nncsv0 = List.map (fun [tid; jid; amin; amax; cmin; cmax; dl; pr;k] ->
-        [tid; jid; amin; amax; cmin; cmax; dl; (string_of_int ((int_of_string dl) - (int_of_string amin)))]) ncsv in
+    let nncsv0 = List.map (fun [tid; jid; amin; amax; cmin; cmax; dl; pr;k] -> if ((int_of_string dl) - (int_of_string amin) > 0) then
+        [tid; jid; amin; amax; cmin; cmax; dl; (string_of_int ((int_of_string dl) - (int_of_string amin)))] else []) ncsv in
     let nncsv = ["Task ID"; "Job ID"; "Arrival min"; "Arrival max"; "Cost min";
     "Cost max"; "Deadline"; "Priority"] :: (nncsv0) in
     let _ = Csv.save "kind.csv" ncsv in
