e58d50694f975aae88dc7cbf3b7443a8c4a4a4b4
Saranya <saranya@Saranyas-MacBook-Pro.local>
2021-01-06 16:59:34 +0100

    runtime lib additions

--- timedc-lib/src/runtime_profile.c
+++ timedc-lib/src/runtime_profile.c
@@ -1,6 +1,20 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <time.h>
+#include <setjmp.h>
+#include <pthread.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <linux/sched.h>
+#include <linux/types.h>
 #include <cillib.h>
+#include <string.h>
+#include <errno.h>
+#include <math.h>
 
 int ktc_swcet(char* fname,  struct timespec* start_time){
     static int i = 0;
