7b292ecef72b50829d110ac3d0e993f1fd23dfce
Saranya <saranya@Saranyas-MacBook-Pro.local>
2021-01-06 16:52:26 +0100

    runtime lib additions

--- timedc-lib/src/runtime_profile.c
+++ timedc-lib/src/runtime_profile.c
@@ -3,11 +3,11 @@
 
 int ktc_swcet(char* fname,  struct timespec* start_time){
     static int i = 0;
-    struct timespec now, exec;
+    struct timespec now, exectm;
 	long tme, newt;
 	clock_gettime(CLOCK_REALTIME, &now);
-	exec = diff_timespec(*start_time, now);
-    newt = timespec_to_unit(exec, -3);
+	exectm = diff_timespec(now, *start_time);
+    newt = timespec_to_unit(exectm, -3);
 	if(newt > tme){
 		tme = newt;
     }
